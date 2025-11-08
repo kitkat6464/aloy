3800b5ace073ea626ae60e5a9a18d98671e91cb3
KitKat <227429303+kitkat6464@users.noreply.github.com>
2025-11-08 19:59:26 +0000

    I forgor to rebuild initramfs

--- build.sh
+++ build.sh
@@ -50,3 +50,9 @@ dnf -y swap zram-generator-defaults cachyos-settings
 # Disable repos    
 dnf -y copr disable bieszczaders/kernel-cachyos
 dnf -y copr disable bieszczaders/kernel-cachyos-addons
+
+# Build initramfs
+KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
+export DRACUT_NO_XATTR=1
+dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree --add fido2 -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
+chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
